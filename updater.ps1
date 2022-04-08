b7e07cfbe169057ec759519036f09d1aab95ca86
Elio-zhy <elio-zhou@outlook.com>
2022-04-08 20:35:12 +0800

    Modify 7z path check

--- updater.ps1
+++ updater.ps1
@@ -1,6 +1,6 @@
 function Check-7z {
     $7zdir = (Get-Location).Path + "\7z"
-    if (-not (Test-Path ($7zdir + "\7za.exe")) -and -not (Test-Path ([Environment]::ExpandEnvironmentVariables("%Programfiles%\7-Zip\7z.exe"))))
+    if (-not (Test-Path ($7zdir + "\7za.exe")) -and -not (Test-Path (Get-Command "7z.exe").Path))
     {
         $download_file = (Get-Location).Path + "\7z.zip"
         Write-Host "Downloading 7z" -ForegroundColor Green
@@ -26,20 +26,6 @@ function Check-PowershellVersion {
     }
 }
 
-function Check-Ytplugin {
-    $ytdlp = Get-ChildItem "yt-dlp*.exe" -ErrorAction Ignore
-    $youtubedl = Get-ChildItem "youtube-dl.exe" -ErrorAction Ignore
-    if ($ytdlp) {
-        return $ytdlp.ToString()
-    }
-    elseif ($youtubedl) {
-        return $youtubedl.ToString()
-    }
-    else {
-        return $null
-    }
-}
-
 function Check-Mpv {
     $mpv = (Get-Location).Path + "\mpv.exe"
     $is_exist = Test-Path $mpv
@@ -52,33 +38,9 @@ function Download-Mpv ($filename, $link) {
     Invoke-WebRequest -Uri $link -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox -OutFile $filename
 }
 
-function Download-Ytplugin ($plugin, $version) {
-    $link = ""
-    $plugin_exe = ""
-    switch -wildcard ($plugin) {
-        "yt-dlp*" {
-            Write-Host "Downloading $plugin ($version)" -ForegroundColor Green
-            $global:progressPreference = 'Continue'
-            $32bit = ""
-            if (-Not (Test-Path (Join-Path $env:windir "SysWow64"))) {
-                $32bit = "_x86"
-            }
-            $link = -join("https://github.com/yt-dlp/yt-dlp/releases/download/", $version, "/", $plugin, $32bit, ".exe")
-            $plugin_exe = -join($plugin, $32bit, ".exe")
-        }
-        "youtube-dl" {
-            Write-Host "Downloading $plugin ($version)" -ForegroundColor Green
-            $global:progressPreference = 'Continue'
-            $link = -join("https://yt-dl.org/downloads/", $version, "/youtube-dl.exe")
-            $plugin_exe = "youtube-dl.exe"
-        }
-    }
-    Invoke-WebRequest -Uri $link -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox -OutFile $plugin_exe
-}
-
 function Extract-Mpv ($file) {
-    if (Test-Path ([Environment]::ExpandEnvironmentVariables("%Programfiles%\7-Zip\7z.exe"))) {
-        & ([Environment]::ExpandEnvironmentVariables("%Programfiles%\7-Zip\7z.exe")) x -y $file | FIND "Extracting archive" | Write-Host -ForegroundColor Green
+    if (Test-Path (Get-Command "7z.exe").Path) {
+        & (Get-Command "7z.exe").Path x -y $file | FIND "Extracting archive" | Write-Host -ForegroundColor Green
     }
     else {
         $7za = (Get-Location).Path + "\7z\7za.exe"
@@ -115,29 +77,6 @@ function Get-Latest-Mpv($Arch, $channel) {
         }
     }
     return $filename, $download_link
-}
-
-function Get-Latest-Ytplugin ($plugin) {
-    switch -wildcard ($plugin) {
-        "yt-dlp*" {
-            $link = "https://github.com/yt-dlp/yt-dlp/releases.atom"
-            Write-Host "Fetching RSS feed for ytp-dlp" -ForegroundColor Green
-            $global:progressPreference = 'silentlyContinue'
-            $resp = [xml](Invoke-WebRequest $link -MaximumRedirection 0 -ErrorAction Ignore -UseBasicParsing).Content
-            $link = $resp.feed.entry[0].link.href
-            $version = $link.split("/")[-1]
-            return $version
-        }
-        "youtube-dl" {
-            $link = "https://yt-dl.org/downloads/latest/youtube-dl.exe"
-            Write-Host "Fetching RSS feed for youtube-dl" -ForegroundColor Green
-            $global:progressPreference = 'silentlyContinue'
-            $resp = Invoke-WebRequest $link -MaximumRedirection 0 -ErrorAction Ignore -UseBasicParsing
-            $redirect_link = $resp.Headers.Location
-            $version = $redirect_link.split("/")[4]
-            return $version
-        }
-    }
 }
 
 function Get-Arch {
@@ -324,38 +263,6 @@ function Upgrade-Mpv {
     Check-Autodelete $remoteName
 }
 
-function Upgrade-Ytplugin {
-    $yt = Check-Ytplugin
-    if ($yt) {
-        $latest_release = Get-Latest-Ytplugin((Get-Item $yt).BaseName)
-        if ((& $yt --version) -match ($latest_release)) {
-            Write-Host "You are already using latest" (Get-Item $yt).BaseName "-- $latest_release" -ForegroundColor Green
-        }
-        else {
-            Write-Host "Newer" (Get-Item $yt).BaseName "build available" -ForegroundColor Green
-            & $yt --update
-        }
-    }
-    else {
-        Write-Host "ytdlp or youtube-dl doesn't exist. " -ForegroundColor Green -NoNewline
-        $result = Read-KeyOrTimeout "Proceed with downloading? [Y/n] (default=n)" "N"
-        Write-Host ""
-
-        if ($result -eq 'Y') {
-            $result_exe = Read-KeyOrTimeout "Download ytdlp or youtubedl? [1=ytdlp/2=youtubedl] (default=1)" "D1"
-            Write-Host ""
-            if ($result_exe -eq 'D1') {
-                $latest_release = Get-Latest-Ytplugin "yt-dlp"
-                Download-Ytplugin "yt-dlp" $latest_release
-            }
-            elseif (($result_exe -eq 'D2')) {
-                $latest_release = Get-Latest-Ytplugin "youtube-dl"
-                Download-Ytplugin "youtube-dl" $latest_release
-            }
-        }
-    }
-}
-
 function Read-KeyOrTimeout ($prompt, $key){
     $seconds = 9
     $startTime = Get-Date
@@ -402,7 +309,6 @@ try {
     # Sourceforge only support TLS 1.2
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     Upgrade-Mpv
-    Upgrade-Ytplugin
     Write-Host "Operation completed" -ForegroundColor Magenta
 }
 catch [System.Exception] {
